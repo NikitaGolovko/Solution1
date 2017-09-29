637f2016d83141755cea2658baf89ca2105461d7
Nikita Golovko <nikita.golovko@commoncensus.com>
2017-09-29 10:27:40 -0400

    Initial Add

---
+++ CBILite/Calculator.cs
@@ -0,0 +1,23 @@
+﻿//using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBILite
+{
+	public class Calculator : Core.ICalc
+	{
+		public int Add(int number1, int number2)
+		{
+			//UnityContainer container = new UnityContainer();
+			return (number1 + number2);
+		}
+
+		public int Substract(int number1, int number2)
+		{
+			return (number1 - number2);
+		}
+	}
+}
